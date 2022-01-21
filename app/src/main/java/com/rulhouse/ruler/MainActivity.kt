615220d21e71db25fc2eb1484e78fcd471de05ce
Jake <z19970615@gmail.com>
2022-01-21 18:31:25 +0800

    1. Get density.

--- app/src/main/java/com/rulhouse/ruler/MainActivity.kt
+++ app/src/main/java/com/rulhouse/ruler/MainActivity.kt
@@ -1,50 +1,36 @@
 package com.rulhouse.ruler
 
 import android.content.pm.ActivityInfo
-import android.icu.util.MeasureUnit
-import android.icu.util.MeasureUnit.CENTIMETER
-import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.rulhouse.ruler.ui.theme.RulerTheme
-import android.util.DisplayMetrics
 import android.util.Log
-import androidx.compose.material.Button
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.rulhouse.ruler.feature_node.presentation.ruler.RulerScreen
 import com.rulhouse.ruler.feature_node.presentation.ruler.util.Screen
 import dagger.hilt.android.AndroidEntryPoint
+import android.os.Build
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setOrientation()
-        setSystemBar()
+        var screenDensity = resources.displayMetrics.density
+        var width = resources.displayMetrics.widthPixels
+        var height = resources.displayMetrics.heightPixels
+        Log.d("testDensity", "Density = $screenDensity, width = $width. height = $height")
 
+//        setOrientation()
+        setSystemBar()
+//
         setContent {
             RulerTheme() {
                 Surface(
@@ -86,5 +72,15 @@ class MainActivity : ComponentActivity() {
 
     private fun hideSystemBar() {
         WindowInsetsControllerCompat(window, window.decorView).hide(WindowInsetsCompat.Type.systemBars())
+    }
+
+    fun getDensity(): Float {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val config = application.resources.configuration
+            config.densityDpi / 160f
+        } else {
+            val metrics = application.resources.displayMetrics
+            metrics.density
+        }
     }
 }
