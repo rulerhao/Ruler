6acb6fe40368ae4e0362b8fab65f1f6055930537
rulerhao <z19970615@gmail.com>
2022-03-28 18:12:47 +0800

    1. Add straight line between touch down and touch up. 2. Delete paint path.

--- app/src/main/java/com/rulhouse/ruler/feature_node/presentation/ruler/GestureScreen.kt
+++ app/src/main/java/com/rulhouse/ruler/feature_node/presentation/ruler/GestureScreen.kt
@@ -2,12 +2,9 @@ package com.rulhouse.ruler.feature_node.presentation.ruler
 
 import android.util.Log
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.geometry.Offset
@@ -30,6 +27,10 @@ fun GestureScreen(
     val ACTION_MOVE = 2
     val ACTION_UP = 3
 
+    val arrowPath: Path = remember { Path() }
+    var startPosition = Offset.Unspecified
+    var endPosition = Offset.Unspecified
+
     val path = remember { Path() }
     var motionEvent by remember { mutableStateOf(ACTION_IDLE) }
     var currentPosition by remember { mutableStateOf(Offset.Unspecified) }
@@ -43,12 +44,14 @@ fun GestureScreen(
                     awaitFirstDown().also {
                         motionEvent = ACTION_DOWN
                         currentPosition = it.position
+                        startPosition = it.position
                     }
 
                     do {
                         val event: PointerEvent = awaitPointerEvent()
                         motionEvent = ACTION_MOVE
                         currentPosition = event.changes.first().position
+                        endPosition = event.changes.first().position
                     } while (event.changes.any { it.pressed })
 
                     motionEvent = ACTION_UP
@@ -65,14 +68,23 @@ fun GestureScreen(
                 path.lineTo(currentPosition.x, currentPosition.y)
             }
             ACTION_UP -> {
-                path.lineTo(currentPosition.x, currentPosition.y)
+                arrowPath.moveTo(startPosition.x, startPosition.y)
+                arrowPath.lineTo(endPosition.x, endPosition.y)
+                path.reset()
             }
             else -> Unit
         }
-
         drawPath(
             color = Color.Red,
             path = path,
+            style = Stroke(
+                width = 4.dp.toPx(),
+                cap = StrokeCap.Round,
+                join = StrokeJoin.Round)
+        )
+        drawPath(
+            color = Color.Green,
+            path = arrowPath,
             style = Stroke(
                 width = 4.dp.toPx(),
                 cap = StrokeCap.Round,
